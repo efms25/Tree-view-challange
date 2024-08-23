1aa9b113041abd4d6fc7b4a5640320393db3f84e
Eric Santana <efms.king@gmail.com>
2024-08-22 22:16:50 -0300

    fix: height full layout

--- src/pages/Home/Home.styles.tsx
+++ src/pages/Home/Home.styles.tsx
@@ -2,18 +2,22 @@ import styled from "styled-components";
 
 const Container = styled.div`
   background: ${({ theme }) => theme.colors.borderCard};
-  min-height: 100vh;
   width: 100vw;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
   position: relative;
 `;
 const BodyContainer = styled.div`
   position: relative;
   display: block;
+  flex: 1 1 auto;
   margin: 16px;
   border-radius: 4px;
   border: solid 1px ${({ theme }) => theme.colors.borderCard};
   background: ${({ theme }) => theme.colors.background};
   padding: 16px;
+
 `;
 const BreadcrumbContainer = styled.div`
     display: flex;
